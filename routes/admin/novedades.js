cae7d71345055f25f370471b38f92c142b9d5bb9
Hernan Sueldo <mayday84@gmail.com>
2022-12-27 19:28:33 -0300

    modulo de busqueda

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -8,7 +8,12 @@ const destroy = util.promisify(cloudinary.uploader.destroy);
 
 router.get('/', async function (req, res, next) {
 
-    var novedades = await novedadesModel.getNovedades();
+    var novedades
+    if (req.query.q === undefined) {
+        novedades = await novedadesModel.getNovedades();
+    } else {
+        novedades = await novedadesModel.buscarNovedades(req.query.q);
+    }
 
     novedades = novedades.map(novedad => {
         if (novedad.img_id) {
@@ -32,7 +37,9 @@ router.get('/', async function (req, res, next) {
     res.render('admin/novedades', {
         layout: 'admin/layout',
         usuario: req.session.nombre,
-        novedades
+        novedades,
+        is_search: req.query.q !== undefined,
+        q: req.query.q
     });
 });
 
